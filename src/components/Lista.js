8b9e4873e1e6e60609a5ac47df0e2123b55e8482
Miguel Gonzalez <miglez90290995@gmail.com>
2017-09-20 18:00:25 -0500

    Mas mejoras en el diseño

--- src/components/Lista.js
+++ src/components/Lista.js
@@ -10,9 +10,11 @@ const w3margin = {
 };
 
 const interno = {
-    margin:20,
+    margin:10,
     padding:'10px 10vw'
 };
+
+
 
 class Lista extends React.Component{
     state = {
@@ -70,37 +72,48 @@ class Lista extends React.Component{
         console.log(items, filtro);
 
         let tituloFiltro = '';
+        let animacion = '';
         switch (filtro){
             case 'SHOW_TODOS':
                 tituloFiltro = 'Lista de artículos';
+                animacion = 'w3-animate-top';
                 break;
             case 'SHOW_TACHADOS':
                 tituloFiltro = 'Artículos comprados';
+                animacion = 'w3-animate-bottom';
                 break;
             case 'SHOW_ACTIVOS':
                 tituloFiltro = 'Artículos por comprar';
+                animacion = 'w3-animate-right';
                 break;
+            default:
+                console.error('Error');
 
         }
+
 
         return(
             <div className="App">
                 <div style={{width:'100vw'}} >
                     <div className="w3-container w3-blue" style={w3margin} >
-                        <h1>Lista de compras</h1>
+                        <h1 className="w3-animate-top">Lista de compras</h1>
                     </div>
-                    <form className="w3-container" style={interno} onSubmit={this.addCompra}>
-                        <label htmlFor="nuevoItem">Nuevo artículo: </label>
-                        <input
-                            placeholder="ej. Arroz"
-                            name="nuevoItem"
-                            className="w3-input"
-                            value={nuevoItem}
-                            onChange={this.handleChange}
-                            type="text" />
+                    <form className="w3-row-padding" style={interno} onSubmit={this.addCompra}>
+                        <div className="w3-twothird">
+                            <label htmlFor="nuevoItem">Nuevo artículo: </label>
+                            <input
+                                placeholder="ej. Arroz"
+                                name="nuevoItem"
+                                className="w3-input"
+                                value={nuevoItem}
+                                onChange={this.handleChange}
+                                type="text" />
 
-                        <br/>
-                        <button className="w3-btn w3-blue" > Enviar </button>
+                        </div>
+                        <div className="w3-third">
+                            <button className="w3-btn w3-blue" > Enviar </button>
+                        </div>
+
                     </form>
                     <div style={interno}>
                         <h3>Filtro: </h3>
@@ -130,28 +143,31 @@ class Lista extends React.Component{
                     </div>
 
                     <div style={interno}>
-                        <h2 style={{color:'#2196F3'}}>{tituloFiltro}</h2>
+                        <h2 className={animacion} style={{color:'#2196F3'}}>{tituloFiltro}</h2>
 
 
-                        <ul>
+                        <ul className="w3-ul w3-card-4">
                             {
                                 items.map(i=>
-                                        <div key={i.key}>
-                            <span
-                                onClick={()=>this.props.comprasActions.checkComprado(i)}
-                                style={{cursor:'pointer'}}
-                                className={i.comprado?"tachado":null}
-                            >{i.text}
-                            </span>
-                                            --
-                                            <a href="!#"
-                                               onClick={()=>this.deleteCompra(i)}
-                                            >borrar
-                                            </a>
-                                        </div>
+                                    <li
+                                        key={i.key}
+                                        className="w3-display-container">
+                                        <span
+                                            onClick={()=>this.props.comprasActions.checkComprado(i)}
+                                            style={{cursor:'pointer'}}
+                                            className={i.comprado?"tachado":null}>
+                                            {i.text}
+                                        </span>
+                                        <span
+                                            onClick={()=>this.deleteCompra(i)}
+                                            className="w3-button w3-display-right">
+                                            &times;
+                                        </span>
+                                    </li>
                                 )
                             }
-                        </ul>
+                            </ul>
+
                     </div>
                 </div>
 
